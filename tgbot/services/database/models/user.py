14100fab3c0954cbeab7804de43f684c740e5abe
PrinterOMG <PrinterOMG@ya.ru>
2023-02-19 14:32:23 +0300

    * Update user database model

--- tgbot/services/database/models/user.py
+++ tgbot/services/database/models/user.py
@@ -1,8 +1,17 @@
-from sqlalchemy import Column, BigInteger, Integer, ForeignKey, DateTime, Boolean
+from sqlalchemy import Column, BigInteger, Integer, ForeignKey, DateTime, Boolean, Table
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.expression import text
 
 from tgbot.services.database.base import Base
+
+
+# Избранные группы
+selected_groups = Table(
+    'selected_group',
+    Base.metadata,
+    Column('user_id', ForeignKey('telegram_user.telegram_id'), primary_key=True),
+    Column('group_id', ForeignKey('group.group_id'), primary_key=True),
+)
 
 
 class User(Base):
@@ -11,9 +20,12 @@ class User(Base):
     telegram_id = Column(BigInteger, primary_key=True, unique=True, autoincrement=False)
     language_id = Column(Integer, ForeignKey('language.id'), nullable=True)
     role_id = Column(Integer, ForeignKey('role.id'), nullable=True)
+    group_id = Column(BigInteger, ForeignKey('group.group_id'), nullable=True)
     created_at = Column(DateTime(), nullable=False, server_default=text('NOW()'))
     is_blocked_bot = Column(Boolean, nullable=False, server_default=text('false'))
     is_blocked = Column(Boolean, nullable=False, server_default=text('false'))
 
-    language = relationship('Language', lazy='selectin', backref='user')
-    role = relationship('Role', lazy='selectin', backref='user')
+    language = relationship('Language', lazy='selectin')
+    role = relationship('Role', lazy='selectin')
+    group = relationship('Group', lazy='selectin')
+    selected_groups = relationship('Group', lazy='selectin', secondary=selected_groups)
