ce9e6c1bc4528e2edce77673bd3517cfa23ae7f5
PrinterOMG <PrinterOMG@ya.ru>
2023-02-22 14:50:39 +0300

    * Refactor teachers

--- tgbot/services/database/models/groupteacher.py
+++ tgbot/services/database/models/groupteacher.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, BigInteger, String, ForeignKey
+from sqlalchemy import Column, BigInteger, String, ForeignKey, select
+from sqlalchemy.orm import relationship
 
 from tgbot.services.database.base import Base
 
@@ -10,3 +11,11 @@ class GroupTeacher(Base):
     teacher_name = Column(String, nullable=False)
     lesson_type = Column(String, nullable=True)
     lesson_name = Column(String, nullable=False)
+
+    group = relationship('Group', lazy='selectin', uselist=False)
+
+    @classmethod
+    async def get_group_teachers(cls, group_id, db_session):
+        async with db_session() as session:
+            records = await session.execute(select(GroupTeacher).where(GroupTeacher.group_id == group_id))
+            return records.scalars().all()
