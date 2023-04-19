2cd9912964272672c570e4cfcfcad79ebfe3f0ee
PrinterOMG <PrinterOMG@ya.ru>
2023-04-19 09:27:55 +0300

    * Add alembic

--- tgbot/misc/callbacks.py
+++ tgbot/misc/callbacks.py
@@ -3,8 +3,10 @@ from aiogram.utils.callback_data import CallbackData
 
 navigation = CallbackData('nav', 'to', 'payload')
 action = CallbackData('act', 'name', 'payload')
+cancel = CallbackData('cancel', 'to', 'payload')
+
 language_choose = CallbackData('lang_chs', 'lang_id', 'code', 'payload')
+group_choose = CallbackData('grp_chs', 'id', 'action', 'payload')  # actions - 'select', 'add', 'remove'
+
 schedule = CallbackData('schedule', 'action', 'payload')
 change_schedule_week = CallbackData('change_week', 'action', 'payload')
-group_choose = CallbackData('grp_chs', 'id', 'action', 'payload')  # actions - 'select', 'add', 'remove'
-cancel = CallbackData('cancel', 'to', 'payload')
