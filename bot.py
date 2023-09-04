98de7c27bb4838930ce4b27fa1c636283e53e9e5
printeromg <PrinterOMG@ya.ru>
2023-09-04 17:32:32 +0300

    Fix send phone button in verification

--- bot.py
+++ bot.py
@@ -71,9 +71,7 @@ async def main():
     async with engine.begin() as conn:
         # await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
-    async_sessionmaker = sessionmaker(
-        engine, expire_on_commit=False, class_=AsyncSession
-    )
+    async_sessionmaker = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
     storage = RedisStorage2() if config.bot.use_redis else MemoryStorage()
     redis = Redis()
