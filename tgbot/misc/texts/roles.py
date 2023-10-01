5a819004f0e89cc03bbefccf2fcf7dc0d907728c
PrinterOMG <PrinterOMG@ya.ru>
2023-10-01 18:09:07 +0300

    Add details in week schedule & some fixes

--- tgbot/misc/texts/roles.py
+++ tgbot/misc/texts/roles.py
@@ -5,3 +5,4 @@ student = _('Student')
 group_leader = _('Group leader')  # Староста
 authorized = _('Authorized')  # Авторизовался в БРС
 verified = _('Verified')  # Подтвердил группу
+admin = _('Admin')
