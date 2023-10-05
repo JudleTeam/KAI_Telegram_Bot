67799e1a8d002f3df880a43092b71c444667517f
PrinterOMG <PrinterOMG@ya.ru>
2023-10-05 21:16:12 +0300

    Add unknown groups check

--- tgbot/handlers/education.py
+++ tgbot/handlers/education.py
@@ -20,22 +20,29 @@ async def show_my_group(call: CallbackQuery):
             return
 
     group = user.kai_user.group
-    inst_add = ' (ВШПИТ)' if group.group_name in (6110, 6111, 6210, 6310) else ''
-
     pinned_text = group.pinned_text or _(messages.missing)
-    await call.message.edit_text(
-        _(messages.my_group_menu).format(
+
+    if str(group.group_name).startswith('8'):
+        faculty = '1 | Технический колледж'
+    else:
+        faculty = str(group.group_name)[0]
+
+    if len(str(group.group_name)) != 4:
+        text = _(messages.unknown_group).format(pinned_text=md.hitalic(pinned_text))
+    else:
+        inst_add = ' (ВШПИТ)' if group.group_name in (6110, 6111, 6210, 6310) else ''
+        text = _(messages.my_group_menu).format(
             year=md.hcode(str(group.group_name)[1]),
-            faculty=md.hcode(str(group.group_name)[0]),
+            faculty=md.hcode(faculty),
             institute=md.hcode(group.institute.name + inst_add),
             departament=md.hcode(group.departament.name),
             speciality=md.hcode(group.speciality.code),
             profile=md.hcode(group.profile.name),
             group_name=md.hcode(group.group_name),
             pinned_text=md.hitalic(pinned_text)
-        ),
-        reply_markup=inline_keyboards.get_my_group_keyboard(_, user)
-    )
+        )
+
+    await call.message.edit_text(text, reply_markup=inline_keyboards.get_my_group_keyboard(_, user))
 
     try:
         await call.answer()
