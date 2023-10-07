d3197e8b85fefc0b5d776f21cb94cffb7552fe59
PrinterOMG <PrinterOMG@ya.ru>
2023-10-07 23:27:06 +0300

    Add broadcast with new homework

--- tgbot/services/utils/other.py
+++ tgbot/services/utils/other.py
@@ -1,5 +1,16 @@
+import asyncio
+import logging
+from typing import Sequence
+
 import phonenumbers
+from aiogram import Bot
+from aiogram.contrib.middlewares.i18n import I18nMiddleware
+from aiogram.utils.exceptions import RetryAfter, BotBlocked, ChatNotFound, UserDeactivated, TelegramAPIError
 from phonenumbers import NumberParseException
+from sqlalchemy.ext.asyncio import async_sessionmaker
+
+from tgbot.services.database.models import User
+from tgbot.services.database.utils import get_user_locale
 
 
 def parse_phone_number(phone_number) -> str | None:
@@ -33,3 +44,41 @@ def parse_phone_number(phone_number) -> str | None:
         return None
 
     return phonenumbers.format_number(parsed_number, phonenumbers.PhoneNumberFormat.E164)
+
+
+async def send_message(bot: Bot, chat_id: int, text: str, **kwargs) -> None:
+    try:
+        await bot.send_message(chat_id, text, **kwargs)
+
+    except RetryAfter as error:
+        await asyncio.sleep(error.timeout)
+        await send_message(bot, chat_id, text)
+
+    except TelegramAPIError as error:
+        logging.error(f'[{chat_id}] Something went wrong with TelegramAPI: {error}')
+
+    except Exception as error:
+        logging.error(f'[{chat_id}] Something went wrong: {error}')
+
+
+async def broadcast_text(
+        _: I18nMiddleware,
+        chats: Sequence[int],
+        text: str,
+        bot: Bot,
+        db: async_sessionmaker,
+        format_kwargs: dict = None,
+        timeout: float = 0.05,
+        **kwargs):
+
+    logging.info('Start broadcasting')
+
+    async with db() as session:
+        for chat_id in chats:
+            user_locale = await get_user_locale(session, chat_id)
+            text_to_send = _.gettext(text, locale=user_locale).format(**format_kwargs)
+            await send_message(bot, chat_id, text_to_send, **kwargs)
+
+            await asyncio.sleep(timeout)
+
+    logging.info('Broadcast completed')
